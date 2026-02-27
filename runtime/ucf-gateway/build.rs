2c58526e366cf20d86663d03e098ec0822aac59b
HUBId <31565807+HUBId@users.noreply.github.com>
2026-02-27 13:31:25 +0100

    Vendor protoc for ucf-gateway build script

--- runtime/ucf-gateway/build.rs
+++ runtime/ucf-gateway/build.rs
@@ -1,5 +1,11 @@
 fn main() {
     println!("cargo:rerun-if-changed=../../proto/ucf_gateway_v1.proto");
+
+    if std::env::var_os("PROTOC").is_none() {
+        let protoc_path = protoc_bin_vendored::protoc_bin_path().expect("resolve vendored protoc");
+        std::env::set_var("PROTOC", protoc_path);
+    }
+
     prost_build::Config::new()
         .compile_protos(&["../../proto/ucf_gateway_v1.proto"], &["../../proto"])
         .expect("compile gateway proto");
